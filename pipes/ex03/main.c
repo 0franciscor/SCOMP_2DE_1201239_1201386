adc59780ae36032615b5e52a2529ba1a3f7f656f
RitaAriana <1201386@isep.ipp.pt>
2022-03-29 17:02:19 +0100

    Exercício 3 - Done mas pretendo fazer de outra maneira ainda

--- pipes/ex03/main.c
+++ pipes/ex03/main.c
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <time.h>
 #include <sys/wait.h>
+#include <string.h>
 
 int main(int argc, char* argv[]) {
     int fd[2];
@@ -10,18 +11,45 @@ int main(int argc, char* argv[]) {
     pid_t p = fork();
     
     if (p>0){
+
+        close(fd[0]);
+    
+        char frase1[] = "Hello World";
+        int size = strlen(frase1);
+        write(fd[1], &size, sizeof(int));
+		write(fd[1], frase1, strlen(frase1));
+		
+		char frase2[] = "Goodbye!";
+        size = strlen(frase2);
+        write(fd[1], &size, sizeof(int));
+		write(fd[1], frase2, strlen(frase2));
+		
+        int status;
+
+		waitpid(p, &status, 0);
+		printf("Child %d exited with value %d.\n", p, WEXITSTATUS(status));
+		
+		close(fd[1]);
+
+    } else if (p==0){
+
         close(fd[1]);
 		
+		char frase1[100];
+		char frase2[100];
+        int size;
+		
+        read(fd[0], &size, sizeof(int));
+        read(fd[0], frase1, sizeof(char) * size);
+		printf("%s\n", frase1);
+
+		read(fd[0], &size, sizeof(int));
+        read(fd[0], frase2, sizeof(char) * size);
+		printf("%s\n", frase2);
 		
 		close(fd[0]);
 		exit(0);
-        
-    } else if (p==0){
-        close(fd[0]);
-		
-		
-		
-		close(fd[1]);
+
     }
 
     return 0;
