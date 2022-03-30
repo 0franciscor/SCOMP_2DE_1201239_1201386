586ea1e368ca0e960f25d4b2247651a6256a1280
RitaAriana <1201386@isep.ipp.pt>
2022-03-30 23:57:44 +0100

    Exercício 3 - Quase Done (3d está incompleto ainda)

--- signals/ex03/main.c
+++ signals/ex03/main.c
@@ -5,22 +5,20 @@
 #include <string.h>
 #include <sys/wait.h>
 
-void handle_USR1 (int signo, siginfo_t *sinfo, void *context) {
-    char mensagem[100];
-	sprintf(mensagem, "SIGUSR1 has been captured and was sent by the process with PID %d\n", sinfo->si_pid);
-	write(STDOUT_FILENO, mensagem, strlen(mensagem));
+void handle_USR1(int signo, siginfo_t *sinfo, void *context) {
+	char info[100];
+	sprintf(info, "SIGUSR1 has been captured and was sent by the process with PID %d\n", sinfo->si_pid);
+	write(STDOUT_FILENO, info, strlen(info));
 }
 
-
-int main (int argc, char *argv[]){
-    struct sigaction act;
-
-    memset(&act, 0, sizeof(struct sigaction));
-    sigemptyset(&act.sa_mask); /* No signals blocked */
-    act.sa_sigaction = handle_USR1;
-    act.sa_flags = SA_SIGINFO;
-
-    sigaction(SIGUSR1, &act, NULL);
-
-    return 0;
+int main(int argc, char* argv[]) {
+	struct sigaction act;
+	
+	memset(&act, 0, sizeof(struct sigaction));
+	sigemptyset(&act.sa_mask);
+	act.sa_sigaction = handle_USR1; /* No signals blocked */
+	act.sa_flags = SA_SIGINFO;
+	sigaction(SIGUSR1, &act, NULL);
+    
+	return 0;
 }
