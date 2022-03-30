22d9b5798555ced01d146837788bf541e633beb4
RitaAriana <1201386@isep.ipp.pt>
2022-03-31 00:58:48 +0100

    Exercício 9 - Done

--- signals/ex09/main.c
+++ signals/ex09/main.c
@@ -6,11 +6,24 @@
 #include <sys/wait.h>
 #include <time.h>
 
+volatile sig_atomic_t terminado = 0;
+
+void handle_signal(int signo, siginfo_t *sinfo, void *context) {
+	terminado = 1;
+}
+
 int main(int argc, char* argv[]) {
+
+    struct sigaction act;
+	memset(&act, 0, sizeof(struct sigaction));
+	sigemptyset(&act.sa_mask);
+	act.sa_flags = SA_SIGINFO;
+	act.sa_sigaction = handle_signal;
+	sigaction(SIGUSR1, &act, NULL);
 
     pid_t p = fork();
     if (p>0){
-        for(int i = 0; i < 4; i++) {
+        for(int i = 1; i < 4; i++) {
             printf("%d seconds passed\n", i);
 			sleep(1);
 		} 
@@ -18,12 +31,18 @@ int main(int argc, char* argv[]) {
 		kill(p, SIGUSR1);
 		wait(NULL);
     } else if (p == 0){
-		 int r = (rand() % 5) + 1;
+		int r = (rand() % 5) + 1;
 		sleep(r); 
         printf("Tarefe B terminada!\n");
-		sleep(3); 
+
+		while(terminado == 0) {
+			pause();
+        }
+
         printf("Tarefe C terminada!\n");
 		exit(0);
+    } else {
+        exit(1);
     }
 
     return 0;
