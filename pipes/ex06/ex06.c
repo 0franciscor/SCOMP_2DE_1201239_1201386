1b8e3848e7182c7c95e0e4c9d45ee9419dbb35ce
FranciscoRedol <1201239@isep.ipp.pt>
2022-04-02 19:53:26 +0100

    ex06 concluido

--- pipes/ex06/ex06.c
+++ pipes/ex06/ex06.c
@@ -12,67 +12,60 @@ struct pipeStruct {
 
 int main(){
 
-    int array1[ARRAY_MAX_SIZE];
-    int array2[ARRAY_MAX_SIZE];
-
-    for(int i = 0; i < ARRAY_MAX_SIZE; i++){array1[i] = 1;array2[i] = 1;}
-
+    struct pipeStruct fStruct;
+    for(int i = 0; i < ARRAY_MAX_SIZE; i++){
+        fStruct.array1[i] = 1;
+        fStruct.array2[i] = 1;
+    }
 
     pid_t p;
     int soma = 0;
     for(int i = 1; i <= 5; i++){
         
-        // Criacao do Pipe para inserir os dois arrays
         int fd[2];
         if(pipe(fd) == -1){
             printf("Error when creating Pipe");
             return -1;
         }
 
-        close(fd[0]);
-        write(fd[1], &array1, sizeof(array1));
-        //write(fd[1], &array2, sizeof(array2));
+        //close(fd[0]); //Closing the unused part of the pipe lead to a broken Pipe.
+        write(fd[1], &fStruct, sizeof(fStruct));
         close(fd[1]);
         
         int fd2[2];
+        if(pipe(fd2) == -1){
+            printf("Error when creating Pipe");
+            return -1;
+        }
         
         p = fork();
         if(p == 0){
+            struct pipeStruct sonStruct;
             close(fd[1]);
-            int arrayReceived1[ARRAY_MAX_SIZE];
-            int arrayReceived2[ARRAY_MAX_SIZE];
-            read(fd[0], &arrayReceived1, sizeof(arrayReceived1));
-            read(fd[0], &arrayReceived2, sizeof(arrayReceived2));
+            read(fd[0], &sonStruct, sizeof(sonStruct));
             close(fd[0]);
 
             int temp = 0;
-            for(int j = i; j < (i*200); j++){
-                temp += arrayReceived1[(j-1)] + arrayReceived2[(j-1)];
+            for(int j = ((i-1) * 200); j < (i *200); j++){
+                temp += sonStruct.array1[i] + sonStruct.array2[i];
             }
-
-            if(pipe(fd2) == -1){
-                printf("Error when creating Pipe");
-                return -1;
-            }
-            
-            printf("chega aqui %d \n", temp);
 
             close(fd2[0]);
             write(fd2[1], &temp, sizeof(int));
             close(fd2[1]);
             
             exit(0);
-        }    
-
+        }
         wait(NULL);
+
         int readValue = 0;
         close(fd2[1]);
         read(fd2[0], &readValue, sizeof(int));
         close(fd2[0]);
+
         soma += readValue;
     }    
     
-
     printf("A soma dos valores é: %d\n", soma);
 
     return 0;
