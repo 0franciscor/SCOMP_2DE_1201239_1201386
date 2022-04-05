5f654f314269b6e80c145fb755e4e6d0a887a587
FranciscoRedol <1201239@isep.ipp.pt>
2022-04-05 13:57:31 +0100

    Ex08 Revertido

--- signals/ex08/ex08.c
+++ signals/ex08/ex08.c
@@ -8,53 +8,55 @@
 int createProcess();
 void handle_signal(int sign);
 
-sig_atomic_t numProcessos = 5;
+sig_atomic_t numProcessos = 0, maxProcessos = 6;
 
 int main(){
 
     struct sigaction act;
     memset(&act, 0, sizeof(struct sigaction));
-    sigemptyset(&act.sa_mask);
-    act.sa_flags = SA_SIGINFO | SA_NOCLDWAIT | SA_NOCLDSTOP;
+    sigemptyset(&act.sa_mask); /* No signals blocked */
     act.sa_handler = handle_signal;
     sigaction(SIGCHLD, &act, NULL);
-    
-    int processIndex = createProcess();
 
-    if(processIndex != -1){
-        int min = processIndex * 200;
-        int max = (processIndex + 5) * 200;
-        printf("Interval is [%d, %d]\n", min, max);
-        
-        pid_t fProcess = getppid();
-        kill(fProcess, 17);
-        exit(0);
-    }
+    for(int i = 0; i < maxProcessos; i++){
+        int processIndex = createProcess();
 
-    while(numProcessos) {
-        pause();
-    }
+        if(processIndex != -1){
+            int min = processIndex * 200;
+            int max = (processIndex + 5) * 200;
+            printf("Interval is [%d, %d]\n", min, max);
+            
+            exit(0);
+        }
 
-    int i = 0;
-    while(i < 5) {
-        wait(NULL);
-        i++;
     }
 
     return 0;
 }
 
-int createProcess(){   
-    pid_t p; 
-    for(int i = 0; i < numProcessos; i++){
+int createProcess(){    
+    pid_t p;
+
+    if(numProcessos <= maxProcessos){
         p = fork();
+
         if(p == 0)
-            return (i+1);
+            return numProcessos;
+        else{
+            int status;
+            pause();
+            waitpid(p, &status, 0);
+        }
+
     }
     return -1;
 }
 
 
 void handle_signal(int sign){
-    numProcessos--;
+    char mensagem[60];
+    sprintf(mensagem, "SIGCHLD signal captured, There are %d processes left.\n", (5-numProcessos));
+    
+	write(STDOUT_FILENO, mensagem, strlen(mensagem));
+    numProcessos++;
 }
