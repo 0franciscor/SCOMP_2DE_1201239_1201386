e2ddf6c4b5fab850ba6f8e3827f87b9ca52d3c17
FranciscoRedol <1201239@isep.ipp.pt>
2022-04-05 03:57:51 +0100

    Ex12 alterado

--- signals/ex12/ex12.c
+++ signals/ex12/ex12.c
@@ -25,12 +25,10 @@ int main(){
 
     pid_t processArray[50];
     pid_t p;
-    int index; 
 
     for(int i = 0; i < 50; i++){
         p = fork();
         if(p == 0){
-            index = i;
             break;
         }
         processArray[i] = p;
@@ -39,13 +37,12 @@ int main(){
     if(p == 0){
         pid_t fProcess = getppid();
 
-        if(index < 25){
-            if(simula1() != 0){
-                kill(fProcess, 10); //USR1
-            } else{
-                kill(fProcess, 12); //USR2
-            }
+        if(simula1() != 0){
+            kill(fProcess, 10); //USR1
+        } else{
+            kill(fProcess, 12); //USR2
         }
+        
         pause();
         if(efficientAlgorithm == 1){
             simula2();
@@ -54,26 +51,28 @@ int main(){
     }
 
     while(finishedProcs < 25) {
-        printf("%d processes finished Simula1\n", finishedProcs);
+        printf("%d - Simula1\n", finishedProcs);
         sleep(1);
     }
 
     if(efficientAlgorithm == 0) {
         printf("Inefficient algorithm!\n");
+
         for(int i = 0; i < 50; i++) {
             kill(processArray[i], 9);
         }
+
     } else {
         act.sa_handler = handleSignalChild;
         sigaction(SIGUSR1, &act, NULL);
-        
+
         for(int i = 0; i < 50; i++) {
             kill(processArray[i], SIGUSR1);
         }
 
         for(int i = 0; i < 50; i++) {
             waitpid(processArray[i], NULL, 0);
-            printf("Process: %d finished task2\n", processArray[i]);
+            printf("Simula 2 - Process: %d\n", processArray[i]);
         }
         
     }
@@ -83,13 +82,11 @@ int main(){
 }
 
 sig_atomic_t simula1(){
-    sleep(1);
-    return 0;
+    return 0; //testing purposes
 }
 
 sig_atomic_t simula2(){
-    sleep(1);
-    return 1;
+    return 1; //testing purposes
 }
 
 void handleSignal(int sig){
