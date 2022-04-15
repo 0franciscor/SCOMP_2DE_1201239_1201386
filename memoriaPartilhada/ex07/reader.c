c9da8b25e5bd902ccbbe00d1df24597d6f1ab263
RitaAriana <1201386@isep.ipp.pt>
2022-04-15 15:20:40 +0100

    Exercício 7 - Done

--- memoriaPartilhada/ex07/reader.c
+++ memoriaPartilhada/ex07/reader.c
@@ -22,20 +22,38 @@ int main(int argc, char* argv[]) {
 	int fd = shm_open("/ex7",O_EXCL|O_RDWR,S_IRUSR|S_IWUSR);
 	
 	if(fd == -1){
-		perror("Erro Memória Partilhada");
+		perror("Creating or opening shared memory failure");
 		return 1;
 	}	
 	
-	ftruncate(fd,size);
+	if(ftruncate(fd,size) == -1) {
+		perror("Truncating shared memory failure");
+		return 2;
+	}
+
 	infoPartilhada = (info*) mmap(NULL, size,PROT_READ|PROT_WRITE,MAP_SHARED, fd, 0);
 	
-
-    for (int i = 0; i < ARRAY_SIZE; i++){
-        printf("Posição %d: %d\n", i, infoPartilhada->numeros[i]);
-    }
+	for(int i = 0; i < ARRAY_SIZE; i++){
+    	printf("Posição %d: %d\n", i, infoPartilhada->numeros[i]);
+  	}
 	
 
-	shm_unlink("/ex7");
+	 if (munmap((void *)infoPartilhada, size) < 0) {
+        printf("Error at munmap()!\n");
+        return 3;
+    }
+
+   
+    if (close(fd) < 0) {
+        printf("Error at close()!\n");
+        return 4;
+    }
+
+    
+    if (shm_unlink("/ex7") < 0) {
+        printf("Error at shm_unlink()!\n");
+        return 5;
+    }
     
 	return 0;
     
--- memoriaPartilhada/ex07/writer.c
+++ memoriaPartilhada/ex07/writer.c
@@ -17,7 +17,7 @@ typedef struct{
 int main(int argc, char* argv[]) {
 
     int size = sizeof(info); 
-	info* infoPartilhada;
+	info *infoPartilhada;
 
 	int fd = shm_open("/ex7",O_CREAT|O_EXCL|O_RDWR,S_IRUSR|S_IWUSR); 
 
@@ -25,31 +25,37 @@ int main(int argc, char* argv[]) {
     // and read/write permissons (O_RDWR), and open for user to read (S_IRUSR) and write (S_IWUSR).
 	
 	if(fd == -1){
-		perror("Memória Partilhada já existe");
+		perror("Creating or opening shared memory failure");
 		shm_unlink("/ex7");
 		return 1;
 	}	
-	
-	if(ftruncate(fd, size) == -1) {
-		perror("Truncating Memória Partilhada Falhou");
-		exit(2);
+
+	if(ftruncate(fd,size) == -1) {
+		perror("Truncating shared memory failure");
+		return 2;
 	}
 
-    if((infoPartilhada = (info*) mmap(NULL, size,PROT_READ|PROT_WRITE,MAP_SHARED, fd, 0) == MAP_FAILED)) {
-		perror("Mapping Memória Partilhada Falhou");
-		exit(3);
-	}
+    infoPartilhada = (info*) mmap(NULL, size,PROT_READ|PROT_WRITE,MAP_SHARED, fd, 0);
+	
 
     // Maps the area in na address decided by the OS (NULL), requring to use all the size, with
     // read and write permissions (PROT_READ | PROT_WRITE) , consistente with open Area is shared (MAP_SHARED), and
     // it refers to the previous open (fd). All memory area will be used so starting from beginning (offset is 0).
 
-    time_t t; 
-    srand((unsigned)time(&t));
+  	srand(time(NULL));
 
-    for (int i = 0; i < ARRAY_SIZE; i++){
-        int randomNumber = (rand() % (20 - 1 + 1)) + 1;
-        *(infoPartilhada->numeros + i) = randomNumber; // escrever na memória partilhada
+  	for(int i = 0; i < ARRAY_SIZE; i++){
+    	infoPartilhada->numeros[i] = rand()%((20+1)-1) + 1;
+  	}
+
+	if (munmap((void *)infoPartilhada, size) < 0) {
+        printf("Error at munmap()!\n");
+        return 3;
+    }
+    
+    if (close(fd) < 0) {
+        printf("Error at close()!\n");
+        return 4;
     }
 
     return 0;
