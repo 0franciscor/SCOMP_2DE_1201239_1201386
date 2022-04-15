3e61dcd784e3dedda345542892ca4436b2260ee5
FranciscoRedol <1201239@isep.ipp.pt>
2022-04-15 22:08:19 +0100

    Ex04 (Requer verificacao extra)

--- memoriaPartilhada/ex02/reader.c
+++ memoriaPartilhada/ex02/reader.c
@@ -16,12 +16,19 @@ int main() {
     sharedStruct *addr;
 
     fd = shm_open("ex02", O_RDWR, S_IRUSR|S_IWUSR);
+
+    if(fd == -1){
+		perror("Shared Memory File already exists.");
+		shm_unlink("/ex02");
+		return -1;
+	}
+
     ftruncate(fd, sizeof(sharedStruct));
     addr = mmap(NULL, sizeof(sharedStruct),PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
 
     printf("Code: %d\n", addr->code);
-    printf("Description %s\n", addr->description);
-    printf("Price %.2f€\n", addr->price);
+    printf("Description: %s\n", addr->description);
+    printf("Price: %.2f€\n", addr->price);
 
     return 0;
 }
--- memoriaPartilhada/ex02/writer.c
+++ memoriaPartilhada/ex02/writer.c
@@ -14,11 +14,19 @@ typedef struct {
 int main(){
 
     int fd;
+    int size = sizeof(sharedStruct);
     sharedStruct *addr;
-    
+
     fd = shm_open("/ex02", O_CREAT|O_EXCL|O_RDWR, S_IRUSR|S_IWUSR);
-    ftruncate (fd, sizeof(sharedStruct));
-    addr = mmap(NULL, sizeof(sharedStruct), PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
+
+    if(fd == -1){
+		perror("Shared Memory File already exists.");
+		shm_unlink("/ex02");
+		return -1;
+	}
+
+    ftruncate (fd, size);
+    addr = mmap(NULL, size, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
 
     printf("Code:\n");
     scanf("%d", &addr->code);
