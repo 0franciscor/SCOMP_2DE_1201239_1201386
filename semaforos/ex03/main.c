0e91e8ddf8e38fce3aa76780fe6e132e31a9dbb3
RitaAriana <1201386@isep.ipp.pt>
2022-05-16 14:55:45 +0100

    Começo da implementação do exercício 3

---
+++ semaforos/ex03/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/mman.h>
+#include <sys/stat.h> 
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <sys/time.h>
+#include <semaphore.h>
+
+#define MAX_STRINGS 50
+#define STRING_SIZE 80
+
+int main(){
+
+    return 0;
+}
